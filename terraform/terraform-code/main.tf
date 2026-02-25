bbb5079b804860385d17a67702cba614d505d5f4
Yogender <yogender027mae@gmail.com>
2026-02-25 10:52:29 +0100

    adding files

---
+++ terraform/terraform-code/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+  backend "gcs" {}
+}
+
+provider "google" {
+  project = var.project_id
+}
+
+variable "project_id" {
+  type = string
+}
+
+resource "google_storage_bucket" "demo" {
+  name          = "${var.project_id}-argo-tf-demo-${random_id.suffix.hex}"
+  location      = "US"
+  force_destroy = true
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
